f7c0d2492aeb332b1a8e2c2d1af126a83a1f7044
Aman Verma <amanvermadirect@gmail.com>
2021-09-08 12:39:00 +0530

    First commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Captain America' },
+  { id: 12, name: 'Ironman' },
+  { id: 13, name: 'Thor' },
+  { id: 14, name: 'Spiderman' },
+  { id: 15, name: 'Hulk' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Mojo Jojo' },
+  { id: 18, name: 'Oggy' },
+  { id: 19, name: 'Batman' },
+  { id: 20, name: 'Superman' }
+];
